408f99d9646c25e5a5a9d6359adce16eefb381a0
Giovani Cancherini <giovani.canquerini@gmail.com>
2022-10-11 17:57:52 -0300

    Estrutura inicial

--- Algoritmo.java
+++ Algoritmo.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Algoritmo {
 
     private Integer variavel;
-    private List<Integer> listaAtual;
+    private List<Integer> lista;
 
     public Algoritmo(String[] arrayNaoTerminais, String[] arrayTerminais, String simboloProducoes, String simboloInicio, String[] arrayConjuntoProducoes) {
         System.out.println("Resolvendo...");
@@ -16,10 +16,9 @@ public class Algoritmo {
         printlista();
     }
 
-
     public void printlista() {
         System.out.print("Lista: ");
-        for (Integer integer : listaAtual) {
+        for (Integer integer : lista) {
             System.out.print(integer+" ");
         }
         System.out.println();
